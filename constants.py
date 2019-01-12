ae579201b75abc96f73566188782da2271fc5330
karan18 <karan.nahata@gmail.com>
2019-01-12 20:00:38 +0530

    commiting current tables

---
+++ constants.py
@@ -0,0 +1,3 @@
+KEY_BANK='bank'
+KEY_YES_BANK='yes'
+KEY_STATEMENT_DIR='statement_dir'
--- upload-statement.py
+++ upload-statement.py
@@ -1,37 +1,46 @@
 import sys
 
 import csv
-import _mysql_exceptions
 from datetime import datetime
-from sqlalchemy.exc import IntegrityError
+
 
 from bank_transaction import BankTransaction
 from db import Db
+import glob
 from config import Config
+from constants import KEY_BANK, KEY_STATEMENT_DIR, KEY_YES_BANK
 
 def persist_bank_statement_to_db():
-    with open('resources/yes/yes-start-01-jan-2018-end-01-jan-2019.csv', mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, skipinitialspace=True)
-        db = Db()
-        c = Config().getConfig()
-        for row in csv_reader:
-            txn = BankTransaction()
+    c = Config().getConfig()
+    statement_dir = c[KEY_BANK][KEY_YES_BANK][KEY_STATEMENT_DIR]
+    print('Bank Statement Dir: {}'.format(statement_dir))
+    for f in glob.glob(statement_dir + '/*.csv'):
+        print('\t - {}'.format(f))
 
-            d = datetime.strptime(row['Txn Date'].strip('\''), c['bank']['yes']['timestamp_format'])
+    for f in glob.glob(statement_dir + '/*.csv'):
+        c.print_line()
+        print('Processing: {}'.format(f))
+        with open(f, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file, skipinitialspace=True)
+            db = Db()
+            for row in csv_reader:
+                txn = BankTransaction()
 
-            txn.txn_date = d.date()
-            txn.txn_datetime = d
-            txn.reference_no = row['Reference No'].strip('\'')
-            txn.description = row['Description']
-            txn.debit = row['Debit Amount']
-            txn.credit = row['Credit Amount']
-            txn.balance = row['Running Balance']
+                d = datetime.strptime(row['Txn Date'].strip('\''), c['bank']['yes']['timestamp_format'])
 
-            txn.bank = c['bank']['yes']['bank_name']
-            db.save(txn)
+                txn.txn_date = d.date()
+                txn.txn_datetime = d
+                txn.reference_no = row['Reference No'].strip('\'')
+                txn.description = row['Description']
+                txn.debit = row['Debit Amount']
+                txn.credit = row['Credit Amount']
+                txn.balance = row['Running Balance']
+
+                txn.bank = c['bank']['yes']['bank_name']
+                db.save(txn)
 
 
-            print('{}-{}-{}-{}-{}'.format(txn.txn_date, txn.reference_no, txn.description, txn.debit, txn.credit))
+                print('{}-{}-{}-{}-{}'.format(txn.txn_date, txn.reference_no, txn.description, txn.debit, txn.credit))
 
 if __name__== "__main__":
     persist_bank_statement_to_db()
